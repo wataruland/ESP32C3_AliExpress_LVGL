8d8f72c602e667e13137ef1ce8820d95efe46e32
wataru <wataruland@gmail.com>
2024-03-27 17:44:46 +0100

    add 2 screen

--- display/MonitorPC/libraries/lv_conf.h
+++ display/MonitorPC/libraries/lv_conf.h
@@ -357,7 +357,7 @@
 #define LV_FONT_MONTSERRAT_10 0
 #define LV_FONT_MONTSERRAT_12 0
 #define LV_FONT_MONTSERRAT_14 1
-#define LV_FONT_MONTSERRAT_16 1
+#define LV_FONT_MONTSERRAT_16 0
 #define LV_FONT_MONTSERRAT_18 0
 #define LV_FONT_MONTSERRAT_20 0
 #define LV_FONT_MONTSERRAT_22 0
@@ -760,5 +760,4 @@
 #endif /*LV_CONF_H*/
 
 #endif /*End of "Content enable"*/
-
 
